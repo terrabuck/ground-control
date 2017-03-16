4384c49dd0edfcf9aa3c599a37c143a85bfa78e7
Ian <ignaz.k@live.de>
2017-03-16 17:01:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function createWindow () {
 
   // and load the index.html of the app.
   win.loadURL(url.format({
-    pathname: path.join(__dirname, 'src/index.html'),
+    pathname: path.join(__dirname, 'src/settings.html'),
     protocol: 'file:',
     slashes: true
   }));
