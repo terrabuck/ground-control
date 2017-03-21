73c2d666809f793adac978dd7aca2e8aeed07a56
Ian321 <ignaz.k@live.de>
2017-03-21 19:21:30 +0100

    Updated the updater

--- src/js/index.js
+++ src/js/index.js
@@ -24,19 +24,19 @@ function loadIframe() {
             if (a.token && a.token !== "") {
                 checkValidToken(a.token).then(res => {
                     if (res) {
-                        if ($("#popFrame").attr('src') !== "https://streamelements.com/dashboard/%F0%9F%A4%94/activity/popout") {
-                            $("#popFrame").attr('src', 'https://streamelements.com/dashboard/%F0%9F%A4%94/activity/popout').css("display", "block");
+                        if ($("#pop_frame").attr('src') !== "https://streamelements.com/dashboard/%F0%9F%A4%94/activity/popout") {
+                            $("#pop_frame").attr('src', 'https://streamelements.com/dashboard/%F0%9F%A4%94/activity/popout').css("display", "block");
                             $("#noToken").css("display", "none");
                             $("#invToken").css("display", "none");
                         }
                     } else {
-                        $("#popFrame").attr('src', '').css("display", "none");
+                        $("#pop_frame").attr('src', '').css("display", "none");
                         $("#noToken").css("display", "none");
                         $("#invToken").css("display", "block");
                     }
                 });
             } else {
-                $("#popFrame").attr('src', '').css("display", "none");
+                $("#pop_frame").attr('src', '').css("display", "none");
                 $("#noToken").css("display", "block");
                 $("#invToken").css("display", "none");
             }
@@ -44,7 +44,7 @@ function loadIframe() {
             console.log(err);
         }
     } else {
-        $("#popFrame").attr('src', '').css("display", "none");
+        $("#pop_frame").attr('src', '').css("display", "none");
         $("#noToken").css("display", "block");
         $("#invToken").css("display", "none");
     }
@@ -65,25 +65,33 @@ function checkValidToken(token) {
 // Update the app
 if (__filename.includes("app.asar") && !(process.argv[1] && process.argv[1].includes("squirrel"))) {
     try {
-        const autoUpdater = remote.autoUpdater;
-        autoUpdater.on('update-availabe', () => {
-            console.log('update available');
+        got.get(pack.build.squirrelWindows.remoteReleases + "RELEASES").then(res => {
+            if (!upToDate(pack.version, res.body.match(/[0-9]*\.[0-9]*\.[0-9]*/g)[0])) {
+                const autoUpdater = remote.autoUpdater;
+                autoUpdater.on('update-availabe', () => {
+                    console.log('update available');
+                });
+                autoUpdater.on('checking-for-update', () => {
+                    console.log('checking-for-update');
+                });
+                autoUpdater.on('update-not-available', () => {
+                    console.log('update-not-available');
+                    $("body").css("overflow", "auto");
+                    $("#frame_updates").css("display", "none");
+                    $("#frame_main").css("display", "block");
+                });
+                autoUpdater.on('update-downloaded', () => {
+                    autoUpdater.quitAndInstall();
+                });
+                autoUpdater.setFeedURL(pack.build.squirrelWindows.remoteReleases);
+                autoUpdater.checkForUpdates();
+                window.autoUpdater = autoUpdater;
+            } else {
+                $("body").css("overflow", "auto");
+                $("#frame_updates").css("display", "none");
+                $("#frame_main").css("display", "block");
+            }
         });
-        autoUpdater.on('checking-for-update', () => {
-            console.log('checking-for-update');
-        });
-        autoUpdater.on('update-not-available', () => {
-            console.log('update-not-available');
-            $("body").css("overflow", "auto");
-            $("#frame_updates").css("display", "none");
-            $("#frame_main").css("display", "block");
-        });
-        autoUpdater.on('update-downloaded', () => {
-            autoUpdater.quitAndInstall();
-        });
-        autoUpdater.setFeedURL(pack.build.squirrelWindows.remoteReleases);
-        autoUpdater.checkForUpdates();
-        window.autoUpdater = autoUpdater;
     } catch(err) {
         console.log(err);
         $("body").css("overflow", "auto");
@@ -94,4 +102,34 @@ if (__filename.includes("app.asar") && !(process.argv[1] && process.argv[1].incl
     $("body").css("overflow", "auto");
     $("#frame_updates").css("display", "none");
     $("#frame_main").css("display", "block");
+}
+
+/**
+ * Returns true if up to date.
+ * @param {string} local 
+ * @param {string} remote 
+ */
+function upToDate(local, remote) {
+    if (!local || !remote || local.length === 0 || remote.length === 0)
+        return false;
+    if (local == remote)
+        return true;
+    if (/^\d+(\.\d+){0,2}$/.test(local) && /^\d+(\.\d+){0,2}$/.test(remote)) {
+        var lparts = local.split('.');
+        while(lparts.length < 3)
+            lparts.push("0");
+        var rparts = remote.split('.');
+        while (rparts.length < 3)
+            rparts.push("0");
+        for (var i=0; i<3; i++) {
+            var l = parseInt(lparts[i], 10);
+            var r = parseInt(rparts[i], 10);
+            if (l === r)
+                continue;
+            return l > r;
+        }
+        return true;
+    } else {
+        return local >= remote;
+    }
 }
