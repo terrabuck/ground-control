601dfd0ee60e35c4f34472b8c1fb587ac92916be
Ian321 <ignaz.k@live.de>
2017-10-02 16:00:08 +0200

    added heap

---
+++ app/src/modules/heap.js
@@ -0,0 +1,75 @@
+const got = require("got");
+
+function atob(str) {
+	return new Buffer(str, 'base64').toString('binary');
+}
+
+class Heap {
+	/**
+	 * @param {string} jwt 
+	 */
+	constructor(jwt) {
+		const identity = JSON.parse(atob(jwt.split(".")[1]))._id;
+		/**
+		 * @param {string} event 
+		 */
+		function sendEvent(event) {
+			var body = JSON.stringify({
+				app_id: 413792583,
+				identity,
+				event,
+				timestamp: new Date().toISOString(),
+				properties: {
+					UsingGC: true
+				}
+			});
+			return new Promise((resolve, reject) => {
+				got.post("https://heapanalytics.com/api/track", {
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body
+				}).then(res => {
+					// console.log(res.body);
+					resolve();
+				}).catch(err => {
+					reject(err);
+				});
+			})
+		}
+		function sendProperties() {
+			var body = JSON.stringify({
+				app_id: 413792583,
+				identity,
+				properties: {
+					UsingGC: true
+				}
+			});
+			got.post("https://heapanalytics.com/api/add_user_properties", {
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body
+			}).then(res => {
+				// console.log(res.body);
+			}).catch(err => {
+				console.error(err);
+			});
+		}
+		this.open = function() {
+			sendEvent("Opened GC");
+			sendProperties();
+		}
+		this.close = function() {
+			return new Promise((resolve) => {
+				sendEvent("Closed GC").then(function() {
+					resolve();
+				}).catch(() => {
+					resolve();
+				});
+			})
+		}
+	}	
+}
+
+module.exports = Heap;
